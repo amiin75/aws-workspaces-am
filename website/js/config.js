e000593691d707e831c6e9d887ef4a447c67450e
amiin75 <55000941+amiin75@users.noreply.github.com>
2019-09-06 17:25:31 -0400

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_9sk2JEOFq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4h656k7j8ido5oet5924sbbia6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_STIppp9kx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'gjh0jlb96msjckl49umjliutp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ljou5fxpqj.execute-api.us-east-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'rajesh.ramaswamy@netenrich.com'
+        email: 'muhammad.amin@visionetsystems.com'
     }
 };
